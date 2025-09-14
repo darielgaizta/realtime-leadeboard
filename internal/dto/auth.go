5af69861e935bf7f41dc91817948f0563e00784f
darielgaizta <darielgaz@gmail.com>
2025-09-14 17:31:46 +0700

    remove unnecessary dto

--- internal/dto/auth.go
+++ internal/dto/auth.go
@@ -6,7 +6,3 @@ type TokenResponse struct {
 	ExpiresIn    int64  `json:"expires_in"`
 	TokenType    string `json:"token_type"`
 }
-
-type RefreshTokenRequest struct {
-	RefreshToken string `json:"refresh_token" validae:"required"`
-}
